7279ddab0de825e9db4370c4defb24be51bfda00
kagutsuchi57 <103054544+kagutsuchi57@users.noreply.github.com>
2022-07-17 10:00:27 +0530

    Update __init__.py

--- Amanda/modules/sql/__init__.py
+++ Amanda/modules/sql/__init__.py
@@ -9,7 +9,7 @@ def start() -> scoped_session:
     engine = create_engine(DB_URI, pool_size=20, max_overflow=0, client_encoding="utf8")
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
-    return scoped_session(sessionmaker(bind=None, class_=<class 'sqlalchemy.orm.session.SESSION'>, autoflush=True, autocommit=False, expire_on_commit=True, info=None, **kw))
+    return scoped_session(sessionmaker(bind=engine, autoflush=True, autocommit=False, expire_on_commit=True, info=None, **kw))
 
 
 BASE = declarative_base()
